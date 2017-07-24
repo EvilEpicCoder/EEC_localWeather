bba852fed9745cd21915043f63a296c5a288a226
EvilEpicCoder <1st.elixile@gmail.com>
2017-07-24 04:05:15 +0300

    Base structure

---
+++ lw.js
@@ -0,0 +1,14 @@
+window.onload=function(){
+
+addEventListener("click",checkButtons,false);
+
+decodeJson();
+}
+function decodeJson(){
+
+}
+function checkButtons(evt){
+	var x=evt.target.innerText;
+	console.log(myVal);
+}
+
